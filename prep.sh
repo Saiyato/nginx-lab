3f0332f3c25c5725bc7beb3e6fec8d507ae3f2e0
Groeneveld <Saiyato@users.noreply.github.com>
2021-12-19 10:53:44 +0100

    Update prep.sh

--- prep.sh
+++ prep.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 # Install prereqs to do basic DNS investigation and auto-generate SSL certs with LetsEncrypt
-apt-get update && apt install -y nginx-extras dnsutils certbot python-certbot-nginx
+apt-get update && apt install -y nginx-extras dnsutils certbot python-certbot-nginx curl
